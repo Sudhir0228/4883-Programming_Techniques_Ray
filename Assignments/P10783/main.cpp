19839c423e3e45ed55478a8d90bb0449b7456769
Sudhir Ray <66984542+Sudhir0228@users.noreply.github.com>
2024-09-07 20:30:04 -0500

    Create main.cpp

---
+++ Assignments/P10783/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { 
+   int T;
+   int a;
+   int b;
+   int sum=0;
+   int j=0;
+   cin>>T;
+   while(T--){
+      sum=0;
+      cin>>a;
+      cin>>b;
+      for(int i=a;i<=b;i++){
+
+         if(i % 2 == 1){
+            sum += i;
+         }
+      }
+      cout<<"Case "<<++j<<": "<<sum<<endl;
+   }
+}  
