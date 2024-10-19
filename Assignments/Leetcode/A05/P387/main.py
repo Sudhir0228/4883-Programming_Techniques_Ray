b9db728e980769830fc80a8eb95116d108ac7070
Sudhir Ray <66984542+Sudhir0228@users.noreply.github.com>
2024-10-19 18:02:40 -0500

    Create main.py

---
+++ Assignments/Leetcode/A05/P387/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstUniqChar(self, word: str) -> int:
+        count = {}
+        for char in word:
+            if char in count:
+                count[char] +=1
+            else:
+                count[char] = 1
+        
+        for i in range(len(word)):
+            if count[word[i]] == 1:
+                return i
+        
+        return -1
+
