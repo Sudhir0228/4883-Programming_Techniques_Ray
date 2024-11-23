7da4430bb3f4a881841020d94e47426e3aec9b0a
Sudhir Ray <66984542+Sudhir0228@users.noreply.github.com>
2024-11-22 21:53:17 -0600

    Update main.py

--- Assignments/Leetcode/A09/main.py
+++ Assignments/Leetcode/A09/main.py
@@ -1,1 +1,19 @@
+class Solution:
+    def outerTrees(self, trees: List[List[int]]) -> List[List[int]]:
+        def cross(p, q, r):
+            return (q[0] - p[0]) * (r[1] - p[1]) - (q[1] - p[1]) * (r[0] - p[0])
+        
+        points = sorted(trees)
+        lower, upper = [], []
 
+        for p in points:
+            while len(lower) >= 2 and cross(lower[-2], lower[-1], p) < 0:
+                lower.pop()
+            lower.append(p)
+
+        for p in reversed(points):
+            while len(upper) >= 2 and cross(upper[-2], upper[-1], p) < 0:
+                upper.pop()
+            upper.append(p)
+
+        return list(set(tuple(p) for p in (lower + upper)))
