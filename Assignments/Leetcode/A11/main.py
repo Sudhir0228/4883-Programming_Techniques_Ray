32fa914ed764cbe9014eae37b058bf8bc34d8762
Sudhir Ray <66984542+Sudhir0228@users.noreply.github.com>
2024-11-24 02:18:21 -0600

    Update main.py

--- Assignments/Leetcode/A11/main.py
+++ Assignments/Leetcode/A11/main.py
@@ -1,1 +1,28 @@
-
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        for a, b in dislikes:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        color = {}
+        
+        def bfs(node):
+            queue = deque([node])
+            color[node] = 0
+            while queue:
+                current = queue.popleft()
+                for neighbor in graph[current]:
+                    if neighbor not in color:
+                        color[neighbor] = 1 - color[current]
+                        queue.append(neighbor)
+                    elif color[neighbor] == color[current]:
+                        return False
+            return True
+        
+        for node in range(1, n + 1):
+            if node not in color and not bfs(node):
+                return False
+        
+        return True
+        
